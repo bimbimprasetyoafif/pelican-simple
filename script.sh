9cef07b15fb57fe87e8df9d01b71a55fe6537059
bimbimprasetyoafif <bimo.prasetyoafif@gmail.com>
2021-06-24 21:42:56 +0700

    fix everything

--- script.sh
+++ script.sh
@@ -7,13 +7,15 @@ echo "ACTOR: ${GITHUB_ACTOR}"
 
 content_folder=${CONTENT_FOLDER:=content}
 config_file=${CONFIG_FILE:=pelicanconf.py}
+requirements_file=${REQUIREMENTS_FILE:=requirements.txt}
 
 echo "Cheking file ======================================"
 if [ -d "${content_folder}/" ]
 then
     echo "content folder is exist."
 else
-    echo "content does not exist. please create one or more content file in content folder"
+    echo "content does not exist."
+    echo "Please create one or more content file in content folder."
     exit 1
 fi
 
@@ -25,8 +27,17 @@ else
     exit 1
 fi
 
+if [ -f "${requirements_file}" ]
+then
+    echo "requirements.txt file is exist."
+else
+    echo "requirements.txt does not exist."
+    echo "Please provide project python dependency."
+    exit 1
+fi
+
 echo 'Installing dependency ============================='
-pip install -r ${REQUIREMENTS_FILE:=requirements.txt}
+pip install -r ${requirements_file}
 
 echo 'Pelican build ====================================='
 echo "content folder: ${content_folder}"
@@ -42,9 +53,10 @@ remote_branch=${GH_PAGES_BRANCH:=gh-pages}
 git remote add deploy "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git remote -v
 
+echo "Pelican Publish ==================================="
 git checkout ${remote_branch} || git checkout --orphan ${remote_branch}
 git add .
-git commit -m "[Automation] deploy with workflow by ${GITHUB_ACTOR}"
+git commit -m "[Automation] deploy with Github Actions from ${GITHUB_ACTOR}"
 git push deploy ${remote_branch} --force
 rm -rf .git
 
